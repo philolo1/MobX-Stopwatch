4a08f38a6c8d4e6ab8b1cf061f10df5f0a64d042
Patrick Klitzke <philologos14@gmail.com>
2016-06-13 17:39:11 +0900

    design changes

--- src/app/TimerStore.js
+++ src/app/TimerStore.js
@@ -54,6 +54,10 @@ export class TimerStore {
     return this.timer.display;
   }
 
+  @computed get hasStarted() {
+    return this.timer.totalMilliSeconds !== 0;
+  }
+
   @action measure() {
     if (!this.isRunning) return;
 
@@ -73,8 +77,24 @@ export class TimerStore {
     return this.laps.length;
   }
 
+  @computed get lapTime() {
+    return this.laps.map((el) => el.totalMilliSeconds)
+      .reduce((x, y) => x + y, 0);
+  }
+
   @action lapTimer() {
-    this.laps.push(new Timer(this.timer.totalMilliSeconds));
+    this.laps.push(new Timer(this.timer.totalMilliSeconds - this.lapTime));
+  }
+
+  @computed get lapData() {
+    const data = [];
+    for (let i = 0; i < this.laps.length; i++) {
+      data.push({
+        lap: this.laps[i],
+        text: `Lap ${i + 1}`,
+      });
+    }
+    return data.reverse();
   }
 
   @action stopTimer() {
--- src/app/main.jsx
+++ src/app/main.jsx
@@ -6,12 +6,50 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 
-const TimerDisplay = ({timer}) => {
+const TimerDisplay = ({timer, text}) => {
   return (
-    <p style={{fontSize: 15}}>
-      {timer.display}
-    </p>
+    <div
+      style={{
+        borderBottom: '1px solid #d9dae0',
+        display: 'flex',
+      }}
+    >
+      <div
+        style={{fontSize: 30,
+          fontFamily: 'HelveticaNeue-UltraLight',
+          color: '#7f8083',
+          padding: 20,
+          flex: 1,
+        }}
+      >
+        {text}
+      </div>
+      <div
+        style={{fontSize: 30,
+          padding: 20,
+          fontFamily: 'HelveticaNeue-UltraLight',
+          color: '#7f8083',
+        }}
+      >
+        {timer.display}
+      </div>
+    </div>
   );
+};
+
+const ButtonStyle = {
+  fontFamily: 'HelveticaNeue-UltraLight',
+  fontSize: 20,
+  width: 72,
+  height: 72,
+  margin: 24,
+  padding: 0,
+  cursor: 'pointer',
+  letterSpacing: 1,
+  border: 0,
+  borderRadius: '50%',
+  outline: 'none',
+  background: 'white',
 };
 
 const Main = observer(({timerStore}) => {
@@ -21,53 +59,74 @@ const Main = observer(({timerStore}) => {
   if (!timerStore.isRunning) {
     secondButton = (
       <button
-        style={{width: 40, height: 40}}
+        style={{...ButtonStyle, color: '#4bd761'}}
         onClick={() => timerStore.startTimer()}
       >
-        Start
+        start
       </button>
     );
 
     firstButton = (
       <button
-        style={{width: 40, height: 40}}
+        style={ButtonStyle}
         onClick={() => timerStore.resetTimer()}
       >
-        Reset
+        reset
       </button>
     );
+
+    if (!timerStore.hasStarted) {
+      firstButton = null;
+    }
   } else {
     secondButton = (
       <button
-        style={{width: 40, height: 40}}
+        style={{...ButtonStyle, color: '#fd3d2a'}}
         onClick={() => timerStore.stopTimer()}
       >
-        Stop
+        stop
       </button>
     );
 
     firstButton = (
       <button
-        style={{width: 40, height: 40}}
+        style={ButtonStyle}
         onClick={() => timerStore.lapTimer()}
       >
-        Lap
+        lap
       </button>
     );
   }
 
   return (
     <div style={{fontSize: 30}}>
-      <p>
-        Display: {timerStore.mainDisplay}
-      </p>
-      {firstButton}
-      {secondButton}
-      <p style={{fontSize: 20}}>
-        Laps:
-      </p>
+      <div
+        style={{
+          background: 'white',
+          height: 120,
+          fontSize: 60,
+          fontFamily: 'HelveticaNeue-UltraLight',
+          border: 'solid #cecfd0',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'}}
+      >
+        {timerStore.mainDisplay}
+      </div>
       <div>
-        {timerStore.laps.reverse().map((el) => <TimerDisplay key={el.id} timer={el} />)}
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          {firstButton}
+          {secondButton}
+        </div>
+        <div>
+          {timerStore.lapData.map((el) =>
+            <TimerDisplay
+              key={el.lap.id}
+              timer={el.lap}
+              text={el.text}
+            />
+            )}
+        </div>
       </div>
     </div>
   );
